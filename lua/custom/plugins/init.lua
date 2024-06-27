f75a9098e9bce622d528eee39c106830fae0ec73
Orokh <guillaume.kaiffer@gmail.com>
2024-06-27 15:56:50 +0200

    Revert "feat: add first custom keymaps"
    This reverts commit 7ae6aaa27fe236d2b68c2ea17b31602c019ea16f.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,9 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
--- Copy to system clipboard
-vim.keymap.set('n', '<leader>y', '"+y')
-vim.keymap.set('v', '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>Y', '"+Y')
-
 return {}
