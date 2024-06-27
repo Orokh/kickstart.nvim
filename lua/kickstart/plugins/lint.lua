0be1c2066ce81538d80425f92ea28626d58e4f04
Orokh <guillaume.kaiffer@gmail.com>
2024-06-27 17:08:43 +0200

    fix: change file type

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,7 +7,7 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        shell = { 'shellcheck' },
+        bash = { 'shellcheck' },
         sql = { 'sqlfluff' },
       }
 
