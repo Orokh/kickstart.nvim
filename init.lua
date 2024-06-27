acc85ec4cb37dc63ed5c818d836ed4fb39bf51a4
Orokh <guillaume.kaiffer@gmail.com>
2024-06-27 14:47:16 +0200

    fix: type in package name

--- init.lua
+++ init.lua
@@ -778,14 +778,14 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catpuccin/nvim',
-    name = 'catpuccin',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catpuccin-mocha'
+      vim.cmd.colorscheme 'catppuccin-mocha'
 
       -- Set transparent background
       vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
