afa9f2eea9008c598d7aa025cd37bf67ddae31ee
Orokh <guillaume.kaiffer@gmail.com>
2024-07-23 17:10:17 +0200

    feat: add keymap to copy to system clipboard

--- init.lua
+++ init.lua
@@ -913,5 +913,7 @@ require('lazy').setup({
   },
 })
 
+require 'custom.mappings'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/mappings.lua
@@ -0,0 +1,4 @@
+-- Copy to system clipboard
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
